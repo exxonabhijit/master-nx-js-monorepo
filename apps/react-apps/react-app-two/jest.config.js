10a139ef9a7b760809f062ee24cd5c7972ee42c6
exxonabhijit <abhijitm2599@gmail.com>
2023-06-03 11:26:48 +0530

    react-app-two project is created

---
+++ apps/react-apps/react-app-two/jest.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = {
+  displayName: 'react-apps-react-app-two',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/react-apps/react-app-two',
+};
---
+++ apps/react-apps/react-app-two/src/app/app.js
@@ -0,0 +1,9 @@
+import NxWelcome from './nx-welcome';
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-apps-react-app-two" />
+    </div>
+  );
+}
+export default App;
